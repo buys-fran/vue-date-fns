75347dc548063c2b5b2009f12cc0396d6f9833f9
Francois Buys <buys.fran@gmail.com>
2019-02-12 15:47:46 +0200

    Add for humans formats
    What
      * Add for humans format that uses distanceInWords.
      * Add a test.
    
    Why
      * So consumers can provide 'for humans' and get the appropriate
      format.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 var formatDate = require("date-fns/format");
+let forHumans = require("date-fns/distance_in_words");
 
 function createDateFilter (defaultFormat, defaultOptions) {
   return function dateFilter (date, format, opts) {
     if (!date) {
       return "";
     }
+
+    if (((/for humans/i).test(format) || (/for humans/i).test(defaultFormat))) {
+      const from = opts && opts.from ? new Date(opts.from) : new Date();
+      return forHumans(from, date, Object.assign(defaultOptions || {}, opts));
+    }
+
     return formatDate(date, format || defaultFormat, Object.assign(defaultOptions || {}, opts));
   };
 }
--- src/index.spec.js
+++ src/index.spec.js
@@ -2,6 +2,7 @@
 
 let Vue = require("vue");
 let formatDate = require("date-fns/format");
+let distanceInWords = require("date-fns/distance_in_words");
 let expect = require("chai").expect;
 let { mount, createLocalVue } = require("@vue/test-utils");
 
@@ -82,6 +83,56 @@ describe("using filter in component", function () {
     });
   });
 
+  describe("with for humans format", function () {
+    it("should use the filter and produce the same output as date-fns/distanceInWords", function () {
+      let date = new Date();
+      let customFormat = "for humans";
+
+      let Wrapper = Vue.extend({
+        template: `<p>{{ myDate | date('${customFormat}') }}</p>`,
+        filters: {
+          date: dateFilter,
+        },
+        data () {
+          return {
+            myDate: date,
+          };
+        },
+      });
+
+      let wrapper = mount(Wrapper);
+      let text = wrapper.find("p").text();
+      expect(text).to.be.ok;
+      expect(text).to.equal(distanceInWords(new Date(), date));
+    });
+  });
+
+  describe("with for humans format and options", function () {
+    it("should use the filter and produce the same output as date-fns/distanceInWords", function () {
+      let date = new Date();
+      let customFormat = "for humans";
+      let addSuffix = true;
+
+      let Wrapper = Vue.extend({
+        template: `<p>{{ myDate | date('${customFormat}', { addSuffix }) }}</p>`,
+        filters: {
+          date: dateFilter,
+        },
+        data () {
+          return {
+            myDate: date,
+            addSuffix: addSuffix,
+          };
+        },
+      });
+
+      let wrapper = mount(Wrapper);
+      let text = wrapper.find("p").text();
+      expect(text).to.be.ok;
+      expect(text).to.equal(distanceInWords(new Date(), date, { addSuffix }));
+    });
+  });
+
   describe("creating own filter with custom format", function () {
     it("should use the filter and produce the same output as date-fns", function () {
       let date = new Date();
@@ -128,6 +179,55 @@ describe("using filter in component", function () {
       let text = wrapper.find("p").text();
       expect(text).to.be.ok;
       expect(text).to.equal(formatDate(date, customFormat, { locale }));
+    });
+  });
+
+  describe("creating own filter with for humans format", function () {
+    it("should use the filter and produce the same output as date-fns/distanceInWords", function () {
+      let date = new Date();
+      let customFormat = "for humans";
+
+      let Wrapper = Vue.extend({
+        template: "<p>{{ myDate | date }}</p>",
+        filters: {
+          date: createDateFilter(customFormat),
+        },
+        data () {
+          return {
+            myDate: date,
+          };
+        },
+      });
+
+      let wrapper = mount(Wrapper);
+      let text = wrapper.find("p").text();
+      expect(text).to.be.ok;
+      expect(text).to.equal(distanceInWords(new Date(), date, customFormat));
+    });
+  });
+
+  describe("creating own filter with for humans format and custom defaults", function () {
+    it("should use the filter and produce the same output as date-fns/distanceInWords", function () {
+      let date = new Date();
+      let customFormat = "for humans";
+      let addSuffix = true;
+
+      let Wrapper = Vue.extend({
+        template: "<p>{{ myDate | date }}</p>",
+        filters: {
+          date: createDateFilter(customFormat, { addSuffix }),
+        },
+        data () {
+          return {
+            myDate: date,
+          };
+        },
+      });
+
+      let wrapper = mount(Wrapper);
+      let text = wrapper.find("p").text();
+      expect(text).to.be.ok;
+      expect(text).to.equal(distanceInWords(new Date(), date, { addSuffix }));
     });
   });
 });
@@ -201,6 +301,50 @@ describe("using global filter", function () {
       let text = wrapper.find("p").text();
       expect(text).to.be.ok;
       expect(text).to.equal(formatDate(date, customFormat, { locale }));
+    });
+  });
+
+  describe("with for humans format", function () {
+    it("should use the filter and produce the same output as date-fns/distanceInWords", function () {
+      let date = new Date();
+      let customFormat = "for humans";
+
+      let Wrapper = localVue.extend({
+        template: `<p>{{ myDate | date('${customFormat}') }}</p>`,
+        data () {
+          return {
+            myDate: date,
+          };
+        },
+      });
+
+      let wrapper = mount(Wrapper, { localVue });
+      let text = wrapper.find("p").text();
+      expect(text).to.be.ok;
+      expect(text).to.equal(distanceInWords(new Date(), date));
+    });
+  });
+
+  describe("with for humans format and options", function () {
+    it("should use the filter and produce the same output as date-fns/distanceInWords", function () {
+      let date = new Date();
+      let customFormat = "for humans";
+      let addSuffix = true;
+
+      let Wrapper = localVue.extend({
+        template: `<p>{{ myDate | date('${customFormat}', { addSuffix }) }}</p>`,
+        data () {
+          return {
+            myDate: date,
+            addSuffix: addSuffix,
+          };
+        },
+      });
+
+      let wrapper = mount(Wrapper, { localVue });
+      let text = wrapper.find("p").text();
+      expect(text).to.be.ok;
+      expect(text).to.equal(distanceInWords(new Date(), date, { addSuffix }));
     });
   });
 });
@@ -277,16 +421,61 @@ describe("using global filter with custom format", function () {
       expect(text).to.equal(formatDate(date, differentCustomFormat, { locale }));
     });
   });
+
+  describe("with for humans format", function () {
+    it("should use the filter and produce the same output as date-fns/distanceInWords", function () {
+      let date = new Date();
+      let differentCustomFormat = "for humans";
+
+      let Wrapper = localVue.extend({
+        template: `<p>{{ myDate | date('${differentCustomFormat}') }}</p>`,
+        data () {
+          return {
+            myDate: date,
+          };
+        },
+      });
+
+      let wrapper = mount(Wrapper, { localVue });
+      let text = wrapper.find("p").text();
+      expect(text).to.be.ok;
+      expect(text).to.equal(distanceInWords(new Date(), date));
+    });
+  });
+
+  describe("with for humans format and options", function () {
+    it("should use the filter and produce the same output as date-fns", function () {
+      let date = new Date();
+      let differentCustomFormat = "for humans";
+      let addSuffix = true;
+
+      let Wrapper = localVue.extend({
+        template: `<p>{{ myDate | date('${differentCustomFormat}', { addSuffix }) }}</p>`,
+        data () {
+          return {
+            myDate: date,
+            addSuffix: addSuffix,
+          };
+        },
+      });
+
+      let wrapper = mount(Wrapper, { addSuffix });
+      let text = wrapper.find("p").text();
+      expect(text).to.be.ok;
+      expect(text).to.equal(distanceInWords(new Date(), date, { addSuffix }));
+    });
+  });
 });
 
 describe("using global filter with custom format and custom defaults", function () {
   let localVue;
   let customFormat = "DD MMMM YYYY";
   let locale = require("date-fns/locale/ar");
+  let addSuffix = true;
 
   beforeEach(function () {
     localVue = createLocalVue();
-    localVue.use(DateFnsPlugin, customFormat, { locale });
+    localVue.use(DateFnsPlugin, customFormat, { locale, addSuffix });
   });
 
   describe("without any options", function () {
@@ -352,6 +541,50 @@ describe("using global filter with custom format and custom defaults", function 
       expect(text).to.equal(formatDate(date, differentCustomFormat, { locale: differentLocale }));
     });
   });
+
+  // describe("with for humans format", function () {
+  // it("should use the filter and produce the same output as date-fns/distanceInWords", function () {
+  //   let date = new Date();
+  //   let differentCustomFormat = "for humans";
+
+  //   let Wrapper = localVue.extend({
+  //     template: `<p>{{ myDate | date('${differentCustomFormat}') }}</p>`,
+  //     data () {
+  //       return {
+  //         myDate: date,
+  //       };
+  //     },
+  //   });
+
+  //   let wrapper = mount(Wrapper, { localVue });
+  //   let text = wrapper.find("p").text();
+  //   expect(text).to.be.ok;
+  //   expect(text).to.equal(distanceInWords(new Date(), date));
+  // });
+  // });
+
+  // describe("with for humans format and options", function () {
+  //   it("should use the filter and produce the same output as date-fns/distanceInWords", function () {
+  //     let date = new Date();
+  //     let differentCustomFormat = "for humans";
+  //     let addSuffix = true;
+
+  //     let Wrapper = localVue.extend({
+  //       template: `<p>{{ myDate | date('${differentCustomFormat}', { addSuffix }) }}</p>`,
+  //       data () {
+  //         return {
+  //           myDate: date,
+  //           addSuffix: addSuffix,
+  //         };
+  //       },
+  //     });
+
+  //     let wrapper = mount(Wrapper, { localVue });
+  //     let text = wrapper.find("p").text();
+  //     expect(text).to.be.ok;
+  //     expect(text).to.equal(distanceInWords(new Date(), date, { addSuffix: addSuffix }));
+  //   });
+  // });
 });
 
 describe("using mixin", function () {
@@ -423,6 +656,57 @@ describe("using mixin", function () {
       let text = wrapper.find("p").text();
       expect(text).to.be.ok;
       expect(text).to.equal(formatDate(date, customFormat, { locale }));
+    });
+  });
+});
+
+describe("for humans format", () => {
+  describe("in components", () => {
+    it("should use the filter and produce the same output as date-fns/distanceInWords", function () {
+      let date = new Date();
+      let customFormat = "for humans";
+
+      let Wrapper = Vue.extend({
+        template: `<p>{{ myDate | date('${customFormat}') }}</p>`,
+        filters: {
+          date: dateFilter,
+        },
+        data () {
+          return {
+            myDate: date,
+          };
+        },
+      });
+
+      let wrapper = mount(Wrapper);
+      let text = wrapper.find("p").text();
+      expect(text).to.be.ok;
+      expect(text).to.equal(distanceInWords(new Date(), date));
+    });
+
+    it("should take from date option and produce the same output as date-fns/distanceInWords", () => {
+      let date = new Date();
+      let customFormat = "for humans";
+      let yesterday = new Date().setDate(new Date().getDate() - 1);
+      console.log(yesterday);
+
+      let Wrapper = Vue.extend({
+        template: `<p>{{ myDate | date('${customFormat}', { from: yesterday }) }}</p>`,
+        filters: {
+          date: dateFilter,
+        },
+        data () {
+          return {
+            myDate: date,
+            yesterday,
+          };
+        },
+      });
+
+      let wrapper = mount(Wrapper);
+      let text = wrapper.find("p").text();
+      expect(text).to.be.ok;
+      expect(text).to.equal(distanceInWords(yesterday, date));
     });
   });
 });
